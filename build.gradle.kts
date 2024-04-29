ecccd80642dc561cf26ff61a162cd452fd70d03d
FOX1224 <103663464+FOX1224@users.noreply.github.com>
2024-04-29 08:21:34 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google() // Add Google's Maven repository
+        // Other repositories if needed
+    }
+    dependencies {
+        classpath(libs.google.services)// Google Services plugin
+    }
+}
+
+
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+}
